48b8180ddc45fe30d2efd46f06f31c9eb3ce712a
Dario Segger <dario.segger@trivago.com>
2017-07-05 18:16:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,99 @@
+# Elasticsearch Analyzer Test
+
+A time measurement script for elasticsearch analysers written in Python.
+
+## Use cases
+
+Elastisearch [analyzers](https://www.elastic.co/guide/en/elasticsearch/reference/current/analysis.html) convert text into tokens which are added to the inverted index for searching.
+An analyzer consist of three blocks: character filters, tokenizers and token filters.
+In this order the analyzer performs operations on the input stream (character filters), then divides the input stream into tokens (tokenizer) and later on it performs some more operations on the tokens (token filters).
+
+This python script executes search queries to the elasticsearch search API an collects the time needed to execute this queries.
+
+### Prerequisites
+
+There are a few things you need, before you can start working. Please make sure you've installed and properly configured the following software:
+
+* [Elasticsearch 5.4.3](https://www.elastic.co/downloads/elasticsearch)
+* [Python 3](https://www.python.org/downloads/)
+* [Python Requests Library:](http://docs.python-requests.org/en/master/) `pip3 install requests`
+* [Python Click Library:](http://click.pocoo.org/5/) `pip3 install click`
+
+Not required but helpful:
+* [Elasticdump](https://www.npmjs.com/package/elasticdump) to dump docs easily into a testing elasticsearch database
+* [Cerebro](https://github.com/lmenezes/cerebro) to create elasticsearch index templates
+
+## Getting Started
+
+Clone this repository.
+```
+git clone https://github.com/unidario/elasticsearch-analyzer-timing.git
+```
+
+Change the directory to `elasticsearch-analyzer-timing`
+```
+cd elasticsearch-analyzer-timing
+```
+
+Create a txt file with example queries in this directory.
+Each query has to be written in a new line inside the txt file.
+
+Start the script by provinding the path to the queries txt file and at least one index.
+
+```
+python3 analyzer_test.py $PATH_TO_QUERY_TXT_FILE -i $index -i $index2
+```
+
+See the [configuration](#Configuration) for all options.
+
+## Configuration
+
+| Option          | Description                                                  | Required | Default     |
+|---------------------------------------------------------------------------------------------------------|
+| `--help`        | Show help message and exit                                   |          |             |
+| `-i`, `--index` | One elasticsearch index to test (can be used multiple times) | yes      |             |
+| `--protocol`    | Hypertext transfer protocol (either `http` or `https`)       | no       | `http`      |
+| `--url`         | The url of elasticsearch                                     | no       | `localhost` |
+| `--port`        | The port number of elasticsearch                             | no       | `9200`      |
+| `-r`, `--runs`  | The amount of executions per query                           | no       | `1`         |
+
+## Output
+
+The script creates the output inside the bash.
+
+The output contains of 4 Elements:
+1. Indices which could not be tested because they don't exist in the database (only if necessary).
+2. Output of all analyzers for each index.
+3. Stats for the executed queries per index.
+4. Stats for the timing, doc amount and size of the index.
+
+Sample Output:
+```
+Indices for which no calculation can be done because they don't exist:
+index_x, index_y
+
+Analyzers:
+|  Index  | Tokenizer |    Token Filters    |    Char Filters     |
+| index_a | standard  | standard, lowercase |                     |
+| index_b |  pattern  |  lowercase, unique  | html_strip, mapping |
+
+Query stats:
+|  Index  | Queries | Repetitions | Total | Successful | Failed |   Success rate    |
+| index_a |    6    |     70      |  420  |    420     |   0    |      100.0 %      |
+| index_b |    6    |     70      |  420  |    350     |   70   | 0.8333333333333 % |
+
+Speed:
+|  Index  |  Docs  | Size [GB] | Average speed [ms] |
+| index_a | 193438 | 0.238148  | 257.3333333333333  |
+| index_a | 386876 | 0.423296  |       312.42       |
+
+```
+
+## Authors
+
+* **Dario Segger** - *Initial work*
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ analyzer_test.py
@@ -0,0 +1,298 @@
+import requests
+import click
+
+number_exec = 1
+
+def calculate_mean(lst):
+    s = 0
+    for item in lst:
+        s += float(item)
+    if len(lst) == 0:
+        return None
+    else:
+        return s / float(len(lst))
+
+
+def check_if_index_exists(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return True
+    else:
+        return False
+
+
+def fetch_analyzer(url, index):
+    analyzers = {}
+    analyser_resp = requests.get(url + '/_settings')
+    analyser_json = analyser_resp.json()
+    data = analyser_json[index]['settings']['index']
+    # if an analyzer exists, fetch analyzer components from response
+    if 'analysis' in data:
+        data = data['analysis']['analyzer']
+        # in case there is more than one analyzer fetch all of them!
+        i = 0
+        for key in data:
+            token_filter = (data[key]['filter'])
+            char_filter = (data[key]['char_filter'])
+            analyzer_item = {'tokenizer': (data[key]['tokenizer']), 'token_filter': '', 'char_filter': ''}
+            for x in range(0, len(token_filter)):
+                analyzer_item['token_filter'] += token_filter[x]
+                if x < (len(token_filter)-1):
+                    analyzer_item['token_filter'] += ', '
+            for x in range(0, len(char_filter)):
+                analyzer_item['char_filter'] += char_filter[x]
+                if x < (len(char_filter) - 1):
+                    analyzer_item['char_filter'] += ', '
+            analyzers[i] = analyzer_item
+            i += 1
+    # if no analyzer exist, standard analyzer is hardcoded as anlayzer
+    else:
+        analyzer_item = {'tokenizer': 'standard', 'token_filter': 'standard, lowercase', 'char_filter': ''}
+        analyzers[0] = analyzer_item
+    return analyzers
+
+
+def fetch_metrics(url, index, queries):
+    successful = 0
+    stats = {}
+    timing = []
+    response_file = open("response_" + index + ".txt", "w")
+    cache_file = open("cache_" + index + ".txt", "w")
+    stats_file = open("stats_" + index + ".txt", "w")
+    url_search = '%s/_search?pretty' % url
+    url_cache = '%s/_cache/clear?pretty' % url
+    url_stat = '%s/_stats' %url
+    # collect doc and size of index
+    stats_resp = requests.get(url_stat)
+    stats_json = stats_resp.json()
+    stats_file.write(str(stats_json))
+    stats_file.write("\n")
+    stats['docs'] = (stats_json['indices'][index]['total']['docs']['count'])
+    stats['size'] = (stats_json['indices'][index]['total']['store']['size_in_bytes'])
+    stats_file.close()
+
+    # execute a request to es for each query as often as number_exec, clear the cache before each execution
+    for q in queries:
+        for x in range(0, number_exec):
+            # clear ES cache
+            cache = requests.post(url_cache)
+            cache_json = cache.json()
+            cache_file.write(str(cache_json))
+            cache_file.write("\n")
+            # execute request for the query
+            response = requests.get(url_search, data='{"profile": true,"query":' + q + '}')
+            response_json = response.json()
+            response_file.write(str(response_json))
+            response_file.write("\n")
+            if response.status_code == 200:
+                x += 1
+                successful += 1
+                response_json = response.json()
+                timing.append(str(response_json['took']))
+    mean_timing = calculate_mean(timing)
+
+    response_file.close()
+    cache_file.close()
+
+    return mean_timing, successful, stats
+
+
+def output(index, time_per_index, successful_per_index, stats_per_index, queries_num, analyzer_per_index, not_existing_indices):
+    print()
+
+    # print indices which does not exist if there are any
+    if not_existing_indices:
+        print("Indices for which no calculation can be done because they don't exist:")
+        for x in not_existing_indices:
+            print(x)
+        print()
+
+    # print analyzers per index
+    print('Analyzers:')
+    index_str = 'Index'
+    index_len = len(index_str)
+    token_str = 'Tokenizer'
+    token_len = len(token_str)
+    token_filter_str = 'Token Filters'
+    token_filter_len = len(token_filter_str)
+    char_filter_str = 'Char Filters'
+    char_filter_len = len(char_filter_str)
+    for ind in index:
+        if len(ind) > index_len:
+            index_len = len(ind)
+        for key in analyzer_per_index[ind]:
+            if len(analyzer_per_index[ind][key]['tokenizer']) > token_len:
+                token_len = len(analyzer_per_index[ind][key]['tokenizer'])
+            if len(analyzer_per_index[ind][key]['token_filter']) > token_filter_len:
+                token_filter_len = len(analyzer_per_index[ind][key]['token_filter'])
+            if len(analyzer_per_index[ind][key]['char_filter']) > char_filter_len:
+                char_filter_len = len(analyzer_per_index[ind][key]['char_filter'])
+    print('| {0:^{4}s} | {1:^{5}s} | {2:^{6}s} | {3:^{7}s} |'.format(index_str,
+                                                                     token_str,
+                                                                     token_filter_str,
+                                                                     char_filter_str,
+                                                                     index_len,
+                                                                     token_len,
+                                                                     token_filter_len,
+                                                                     char_filter_len))
+    for ind in index:
+        for key in analyzer_per_index[ind]:
+            print('| {0:^{4}s} | {1:^{5}s} | {2:^{6}s} | {3:^{7}s} |'.format(ind,
+                                                                             analyzer_per_index[ind][key]['tokenizer'],
+                                                                             analyzer_per_index[ind][key][
+                                                                                 'token_filter'],
+                                                                             analyzer_per_index[ind][key][
+                                                                                 'char_filter'],
+                                                                             index_len,
+                                                                             token_len,
+                                                                             token_filter_len,
+                                                                             char_filter_len))
+
+    print()
+
+    # calculate & print query stats per index
+    print('Query stats:')
+    failed = {}
+    suc_rate = {}
+    query_str = 'Queries'
+    query_len = len(query_str)
+    rep_str = 'Repetitions'
+    rep_len = len(rep_str)
+    total_str = 'Total'
+    total_len = len(total_str)
+    suc_str = 'Successful'
+    suc_len = len(suc_str)
+    failed_str = 'Failed'
+    failed_len = len(failed_str)
+    suc_rate_str = 'Success rate'
+    suc_rate_len = len(suc_rate_str)
+    total = queries_num * number_exec
+    if len(str(number_exec)) > rep_len:
+        rep_len = len(str(number_exec))
+    if len(str(total)) > total_len:
+        total_len = len(str(total))
+    for ind in index:
+        failed[ind] = total - successful_per_index[ind]
+        if total != 0:
+            suc_rate[ind] = successful_per_index[ind] / total * 100
+        else:
+            suc_rate[ind] = 0
+        if len(ind) > index_len:
+            index_len = len(ind)
+        if len(str(total)) > total_len:
+            total_len = len(str(total))
+        if len(str(successful_per_index[ind])) > suc_len:
+            suc_len = len(str(successful_per_index[ind]))
+        if len(str(failed[ind])) > failed_len:
+            failed_len = len(str(failed[ind]))
+        if len(str(suc_rate[ind])) + 2 > suc_rate_len:
+            suc_rate_len = len(str(suc_rate[ind])) + 2
+    print(
+        '| {0:^{7}s} | {1:^{8}s} | {2:^{9}s} | {3:^{10}s} | {4:^{11}s} | {5:^{12}s} | {6:^{13}s} |'.format(index_str,
+                                                                                                           query_str,
+                                                                                                           rep_str,
+                                                                                                           total_str,
+                                                                                                           suc_str,
+                                                                                                           failed_str,
+                                                                                                           suc_rate_str,
+                                                                                                           index_len,
+                                                                                                           query_len,
+                                                                                                           rep_len,
+                                                                                                           total_len,
+                                                                                                           suc_len,
+                                                                                                           failed_len,
+                                                                                                           suc_rate_len))
+    for ind in index:
+        print('| {0:^{7}s} | {1:^{8}d} | {2:^{9}d} | {3:^{10}d} | {4:^{11}d} | {5:^{12}d} | {6:^{13}s} |'.format(ind,
+                                                                                                                 queries_num,
+                                                                                                                 number_exec,
+                                                                                                                 total,
+                                                                                                                 successful_per_index[
+                                                                                                                     ind],
+                                                                                                                 failed[
+                                                                                                                     ind],
+                                                                                                                 '%s %%' %
+                                                                                                                 suc_rate[
+                                                                                                                     ind],
+                                                                                                                 index_len,
+                                                                                                                 query_len,
+                                                                                                                 rep_len,
+                                                                                                                 total_len,
+                                                                                                                 suc_len,
+                                                                                                                 failed_len,
+                                                                                                                 suc_rate_len))
+    print()
+
+    # print number of docs, size of index and average speed
+    print("Speed:")
+    timing = {}
+    docs_str = 'Docs'
+    docs_len = len(docs_str)
+    size_str = 'Size [GB]'
+    size_len = len(size_str)
+    avg_speed_str = 'Average speed [ms]'
+    avg_speed_len = len(avg_speed_str)
+    for ind in index:
+        if len(str(stats_per_index[ind]['docs'])) > docs_len:
+            docs_len = len(str(stats_per_index[ind]['docs']))
+        if len(str(stats_per_index[ind]['size'])) > size_len:
+            size_len = len(str(stats_per_index[ind]['size']))
+    print(
+        '| {0:^{4}s} | {1:^{5}s} | {2:^{6}s} | {3:^{7}s} |'.format(index_str, docs_str, size_str, avg_speed_str,
+                                                                   index_len, docs_len,
+                                                                   size_len, avg_speed_len))
+    for ind in index:
+        print('| {0:^{4}s} | {1:^{5}d} | {2:^{6}f} | {3:^{7}s} |'.format(ind, stats_per_index[ind]['docs'],
+                                                                         stats_per_index[ind]['size'] * 0.000000001,
+                                                                         str(time_per_index[ind]),
+                                                                         index_len, docs_len,
+                                                                         size_len, avg_speed_len))
+    print()
+
+
+@click.command()
+@click.argument('queries_location')
+@click.option('--index', '-i', multiple=True, help='The index to test (no wildcards with * allowed).')
+@click.option('--protocol', default="http", help='Hypertext transfer protocol. Either http or https (default: http).')
+@click.option('--url', default="localhost", help='The url of elasticsearch (default: localhost).')
+@click.option('--port', default=9200, help='The port number of elasticsearch (default: 9200).')
+@click.option('--runs', '-r', default=1, help='The amount of repetitions per query.')
+def initiation(queries_location, index, protocol, url, port, runs):
+    global number_exec
+
+    url_dict = {}
+    number_exec = runs
+    index = list(index)
+    if len(index) == 0:
+        print('No index specified. Please use --index foo or -i foo to specify an index.')
+    else:
+        # create urls and check if index exists
+        not_existing_indices = []
+        for ind in index:
+            if check_if_index_exists('%s://%s:%s/%s' %(protocol, url, str(port), ind)):
+                url_dict[ind] = '%s://%s:%s/%s' %(protocol, url, str(port), ind)
+            else:
+                not_existing_indices.append(ind)
+        # remove not existing indices from index list
+        if not_existing_indices:
+            for x in not_existing_indices:
+                index.remove(x)
+        # parse queries
+        with open(queries_location) as f:
+            query_list = f.readlines()
+        # removes whitespace chars (e.g. '\n') and leave empty entry
+        query_list = [x.strip() for x in query_list]
+        # remove empty entry in query_list
+        query_list = [x for x in query_list if x]
+        timing_per_index = {}
+        successful_per_index = {}
+        stats_per_index = {}
+        analyzer_per_index = {}
+        for ind in url_dict:
+            timing_per_index[ind], successful_per_index[ind], stats_per_index[ind] = fetch_metrics(url_dict[ind], ind, query_list)
+            analyzer_per_index[ind] = fetch_analyzer(url_dict[ind], ind)
+        output(index, timing_per_index, successful_per_index, stats_per_index, len(query_list), analyzer_per_index, not_existing_indices)
+
+
+if __name__ == '__main__':
+    initiation()
